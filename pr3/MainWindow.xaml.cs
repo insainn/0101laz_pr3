867fa6c8defc0342de35a8b2468478b915a116e3
Ivan <klarfi999000333444@gmail.com>
2026-09-23 11:03:05 +0500

    Разработанный метод UserInfoPlayer. Лазарев

--- pr3/MainWindow.xaml.cs
+++ pr3/MainWindow.xaml.cs
@@ -20,9 +20,36 @@ namespace pr3
     /// </summary>
     public partial class MainWindow : Window
     {
+        public Classes.PersonInfo Player = new Classes.PersonInfo("Student", 100, 10, 1, 0, 0, 5);
         public MainWindow()
         {
             InitializeComponent();
+            UserInfoPlayer();
         }
+        /// <summary> Повышение уровня и обновление данных на UI
+        public void UserInfoPlayer()
+        {
+            // Если уровень персонажа больше чем 100 * уровень персонажа
+            if (Player.Glasses > 100 * Player.Level)
+            {
+                // Увеличиваем уровень на 1
+                Player.Level++;
+                // Обновляем очки уровня
+                Player.Glasses = 0;
+                // Увеличиваем здоровье на 100
+                Player.Health += 100;
+                // Увеличиваем урон на 1
+                Player.Damage++;
+                // Увеличиваем броню на 1
+                Player.Armor++;
+            }
+            // выводим данные на экран
+            playerHealth.Content = "Жизненные показатели: " + Player.Health;
+            playerArmor.Content = "Броня: " + Player.Armor;
+            playerLevel.Content = "Уровень: " + Player.Level;
+            playerGlasses.Content = "Опыт: " + Player.Glasses;
+            playerMoney.Content = "Монеты: " + Player.Money;
+        }
+
     }
 }
